f311d40481a2d6c734cad054b3bb01cefd6cf7bf
Grant Skipper <grantskipper@149-161-178-215.dhcp-bl.indiana.edu>
2018-09-19 14:41:29 -0400

    undo supervisor, rebuilt flange

--- run.sh
+++ run.sh
@@ -11,7 +11,7 @@ sudo service lldpd start -i eth0
 echo "Flange  : `hostname --ip-address`"
 
 # Start flanged, configured by conf file. Mount it to container for more flexible configuation.
-#flanged -c '/etc/flanged/flanged_conf.ini'
+flanged -c '/etc/flanged/flanged_conf.ini'
 
 
-tail -f /var/log/flanged.log
+#tail -f /var/log/flanged.log
